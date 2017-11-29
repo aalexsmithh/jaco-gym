dbfd6b2cde1510b70e350c03807f50082fa87fc5
Alex Smith <alexsmith1@me.com>
2017-11-29 00:29:26 -0500

    more gym gazebo integration done

--- jaco_gym/envs/jaco_env.py
+++ jaco_gym/envs/jaco_env.py
@@ -12,6 +12,10 @@ from gym.utils import seeding
 from jaco_gym.envs import gazebo_env
 
 from sensor_msgs.msg import Image
+from control_msgs.msg import JointControllerState
+from std_msgs.msg import Float64
+
+from std_srvs.srv import Empty
 
 class JacoEnv(gazebo_env.GazeboEnv):
 	metadata = {'render.modes': ['human']}
@@ -20,7 +24,17 @@ class JacoEnv(gazebo_env.GazeboEnv):
 		# run the jaco-arm launch file
 		gazebo_env.GazeboEnv.__init__(self, "JacoArmWithBallAndCup_v0.launch")
 
-		# self.pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=10)
+		# action space
+		self.joint_0_pub = rospy.Publisher('/jaco/jaco_arm_0_joint_position_controller/command', Float64, queue_size=10)
+		self.joint_1_pub = rospy.Publisher('/jaco/jaco_arm_1_joint_position_controller/command', Float64, queue_size=10)
+		self.joint_2_pub = rospy.Publisher('/jaco/jaco_arm_2_joint_position_controller/command', Float64, queue_size=10)
+		self.joint_3_pub = rospy.Publisher('/jaco/jaco_arm_3_joint_position_controller/command', Float64, queue_size=10)
+		self.joint_4_pub = rospy.Publisher('/jaco/jaco_arm_4_joint_position_controller/command', Float64, queue_size=10)
+		self.joint_5_pub = rospy.Publisher('/jaco/jaco_arm_5_joint_position_controller/command', Float64, queue_size=10)
+
+		self.finger_0_pub = rospy.Publisher('/jaco/jaco_finger_joint_0_position_controller/command', Float64, queue_size=10)
+		self.finger_2_pub = rospy.Publisher('/jaco/jaco_finger_joint_2_position_controller/command', Float64, queue_size=10)
+		self.finger_4_pub = rospy.Publisher('/jaco/jaco_finger_joint_4_position_controller/command', Float64, queue_size=10)
 
 		# gym init settings
 		#### THIS WILL NEED TO BE CHANGED TO A DIFFERENT SPACE!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -38,7 +52,48 @@ class JacoEnv(gazebo_env.GazeboEnv):
 
 	def _step(self, action):
 		#control the robot based on the action var
-		return 0
+		rospy.wait_for_service('/gazebo/unpause_physics')
+		try:
+			self.unpause()
+		except (rospy.ServiceException) as e:
+			print ("/gazebo/unpause_physics service call failed")
+
+		
+
+		################## get state
+		rospy.wait_for_service('/gazebo/pause_physics')
+		try:
+			#resp_pause = pause.call()
+			self.pause()
+		except (rospy.ServiceException) as e:
+			print ("/gazebo/pause_physics service call failed")
+
+		for arm in xrange(5):
+			data = None
+			arm_to_check = '/jaco/jaco_arm_{}_joint_position_controller/'.format(arm)
+			print 'checking', arm_to_check
+			while data is not None:
+				try:
+					data = rospy.wait_for_message(arm_to_check, JointControllerState, timeout=5).command
+				except:
+					pass
+			print data
+			exit()
+			state.append(data)
+
+		for finger in xrange(0,5,2):
+			data = None
+			finger_to_check = '/jaco/jaco_arm_{}_joint_position_controller/'.format(finger)
+			while data is not None:
+				try:
+					data = rospy.wait_for_message(finger_to_check, JointControllerState, timeout=5).command
+				except:
+					pass
+
+			state.append(data)
+
+		state = []
+		return state
 
 	def _reset(self):
 		# Resets the state of the environment and returns an initial observation.
@@ -54,7 +109,7 @@ class JacoEnv(gazebo_env.GazeboEnv):
 		return state
 
 	def _pause(self, msg):
-	    programPause = raw_input(str(msg))
+		programPause = raw_input(str(msg))
 
 	def _render(self, mode='human', close=False):
 		# Not sure if this is necessary to define as we are using gazebo for rendering.
